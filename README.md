b0ffb201c1bee858cdb3ff5d45a29491cbdede87
Ajith Antony <ajith.antony@gmail.com>
2012-05-10 06:03:43 -0500

    added sign script and another readme

---
+++ README.md
@@ -0,0 +1,13 @@
+Amazon Reviews for Safari
+==========================
+
+Adds a popup to view amazon review for books while you are browsing [Safari Book Online][1]
+
+I stole most of it from [ The Camelizer][2].
+
+Download here:
+https://github.com/ajithhub/safari_amzn_reviews/blob/master/dist/safari_extension.crx?raw=true
+
+[1] http://my.safaribooksonline.com/
+[2] http://camelcamelcamel.com/camelizer
+
---
+++ sign_package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+pub=$1
+sig=$2
+zip=$3
+crx=$4
+
+byte_swap () {
+  # Take "abcdefgh" and return it as "ghefcdab"
+  echo "${1:6:2}${1:4:2}${1:2:2}${1:0:2}"
+}
+
+crmagic_hex="4372 3234" # Cr24
+version_hex="0200 0000" # 2
+pub_len_hex=$(byte_swap $(printf '%08x\n' $(ls -l "$pub" | awk '{print $5}')))
+sig_len_hex=$(byte_swap $(printf '%08x\n' $(ls -l "$sig" | awk '{print $5}')))
+(
+  echo "$crmagic_hex $version_hex $pub_len_hex $sig_len_hex" | xxd -r -p
+  cat "$pub" "$sig" "$zip"
+) > "$crx"
+echo "Wrote $crx"
