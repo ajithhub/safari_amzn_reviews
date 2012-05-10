986e0af8fb890070168191e0065c88488d3f44d9
Ajith Antony <ajith.antony@gmail.com>
2012-05-10 04:45:52 -0500

    some refactoring, handling multiple emage sets and some errors

--- overlay.js
+++ overlay.js
@@ -86,41 +86,6 @@ var camelizer = {
     return(gBrowser.selectedBrowser.contentDocument);
   },
   
-  getDesiredChartHeight: function() {
-    content_box = document.getElementById("camelizerContentBox");
-    url_box = document.getElementById("camelizerUrlParent");
-    height = content_box.clientHeight - url_box.clientHeight - 2 - 30;
-    
-    if (height <= 0)
-      height = camelizer.last_height;
-    else
-      camelizer.last_height = height;
-    
-    return(height);
-  },
-  
-  getDesiredChartWidth: function() {
-    content_box = document.getElementById("camelizerContentBox");
-    width = content_box.clientWidth - 2 - 30;
-    
-    if (width <= 0)
-      width = camelizer.last_width;
-    else
-      camelizer.last_width = width;
-    
-    return(width);
-  },
-  
-  setChartSize: function()
-  {
-    height = camelizer.getDesiredChartHeight();
-    width = camelizer.getDesiredChartWidth();
-    chart = document.getElementById("camelizerChart");
-    chart.style.height = String(height) + "px";
-    chart.style.width = String(width) + "px";
-    
-    return(camelizer.context.changeImageDimensions(width, height));
-  },
   
   // event handlers
   
@@ -208,13 +173,6 @@ var camelizer = {
       camelizer.displayLoading(camelizer.context);
   },
   
-  onChartLoaded: function(e)
-  {
-    img = this;
-    ctx = img.context;
-    chart = document.getElementById("camelizerChart");
-    camelizer.displayChart(ctx);
-  },
   
   toggleChartPopout: function(e) {
     obj = document.getElementById("camelizerContentBox");
@@ -241,18 +199,22 @@ var camelizer = {
         }
         catch (e)
         {
-          ctx.product_data = {"result_code":1}
+          ctx.product_data = {"result_code": R_ERROR}
         }
         
         if (xhr.status != 200){
           ctx.product_data = {"result_code": R_ERROR};
            console.log("REsponse was not good")
         }
-        
-        console.log("REsponse was good")
-        console.log(xhr.responseText)
-        console.log(ctx.product_data.ItemLookupResponse.Items.Item.CustomerReviews.IFrameURL)
-        console.log(ctx.product_data.ItemLookupResponse.Items.Item.ASIN)
+
+        if (ctx.product_data.ItemLookupResponse.Items.Request.Errors){
+            console.log(ctx.product_data.ItemLookupResponse.Items.Request.Errors.Error.Message);
+            ctx.product_data.result_code =  R_ERROR;
+            console.log(ctx.product_data.result_code);
+        } else {
+          ctx.product_data.result_code =  R_PRODUCT_LOADED;
+
+        }
         camelizer.updateDisplay(ctx);
       }
     };
@@ -261,7 +223,6 @@ var camelizer = {
   hideAllDisplays: function() {
     document.getElementById("camelizerDisplayLoading").style.display = "none";
     document.getElementById("camelizerDisplayError").style.display = "none";
-    document.getElementById("camelizerDisplayChart").style.visibility = "hidden";
     document.getElementById("camelizerDisplayNotSupported").style.display = "none";
     document.getElementById("camelizerDisplayProductNotFound").style.display = "none";
   },
@@ -275,23 +236,6 @@ var camelizer = {
   },
   
   updateDisplay: function(ctx) {
-    var frame = document.createElement('iframe');
-
-    frame.setAttribute('width', '100%');
-    frame.setAttribute('height', '100%');
-    frame.setAttribute('frameborder', '0');
-    frame.setAttribute('id', 'rtmframe');
-    frame.setAttribute('src', ctx.product_data.ItemLookupResponse.Items.Item.CustomerReviews.IFrameURL);
-    var link = document.createElement('a');
-    link.setAttribute('href', ctx.product_data.ItemLookupResponse.Items.Item.DetailPageURL);
-    link.innerHTML= ctx.product_data.ItemLookupResponse.Items.Item.ItemAttributes.Title;
-    var image= document.createElement('img');
-    image.setAttribute('src', ctx.product_data.ItemLookupResponse.Items.Item.ImageSets.ImageSet.TinyImage.URL);
-    document.body.appendChild(image);
-    document.body.appendChild(link);
-    document.body.appendChild(frame);
-    camelizer.hideAllDisplays();
-    return
 
 
     if (!ctx && !(ctx = camelizer_urls[camelizer.getActiveContextUrl()]))
@@ -300,23 +244,41 @@ var camelizer = {
     if (!camelizer.isActiveContext(ctx))
       return;
     
+
     switch (ctx.product_data.result_code)
     {
     case R_PRODUCT_LOADED:
-      if (!ctx.chart_image)
-      {
-        ctx.chart_image = new Image();
-        ctx.chart_image.context = ctx;
-        ctx.chart_image.onload = camelizer.onChartLoaded;
-      }
-      
-      if (ctx.chart_image.src != ctx.product_data.chart_url)
-      {
-        camelizer.displayLoading(ctx, "Product data downloaded, loading chart image...");
-        ctx.chart_image.src = ctx.product_data.chart_url;
-      }
-      else
-        camelizer.displayChart(ctx);
+        var frame = document.createElement('iframe');
+        frame.setAttribute('width', '100%');
+        frame.setAttribute('height', '100%');
+        frame.setAttribute('frameborder', '0');
+        frame.setAttribute('id', 'rtmframe');
+        frame.setAttribute('src', ctx.product_data.ItemLookupResponse.Items.Item.CustomerReviews.IFrameURL);
+
+        var link = document.createElement('a');
+        link.setAttribute('href',
+                ctx.product_data.ItemLookupResponse.Items.Item.DetailPageURL);
+        link.innerHTML =
+            ctx.product_data.ItemLookupResponse.Items.Item.ItemAttributes.Title;
+
+        var image= document.createElement('img');
+
+        var image_sets = ctx.product_data.ItemLookupResponse.Items.Item.ImageSets;
+        var image_set;
+
+        // If there is more than one image set use the first one
+        if (image_sets.ImageSet instanceof Array) {
+            image_set = image_sets.ImageSet[0];
+        } else {
+            image_set = image_sets.ImageSet;
+        }
+
+        image.setAttribute('src', image_set.TinyImage.URL);
+
+        document.body.appendChild(image);
+        document.body.appendChild(link);
+        document.body.appendChild(frame);
+        camelizer.hideAllDisplays();
       
     break;
     
@@ -332,27 +294,6 @@ var camelizer = {
       camelizer.displayProductNotFound(ctx);
     break;
     }
-  },
-  
-  displayChart: function(ctx) {
-    if (!camelizer.isActiveContext(ctx))
-      return;
-    
-    camelizer.hideAllDisplays();
-    chart = document.getElementById("camelizerChart");
-    
-    if (ctx.chart_image.src != chart.src)
-    {
-      chart.src = ctx.chart_image.src;
-      url_elm = document.getElementById("camelizerUrl");
-      url_elm.href = ctx.product_data.product_url;
-      url_elm.innerHTML = ctx.product_data.link_phrase;
-      url_elm = document.getElementById("camelizerFeedbackUrl");
-      url_elm.href = ctx.product_data.feedback_url;
-    }
-    
-    camelizer.setChartSize();
-    camelizer.showDisplay("camelizerDisplayChart");
   },
   
   displayError: function(ctx) {
@@ -391,18 +332,4 @@ var camelizer = {
   setLoadingText: function(msg) {
     document.getElementById("camelizerLoadingText").innerHTML = msg;
   },
-  
-  openProductPage: function() {
-    ctx = camelizer.context;
-    camelizer.createNewSelectedTab(ctx.product_data.product_url);
-  },
-  
-  openContactPage: function() {
-    ctx = camelizer.context;
-    camelizer.createNewSelectedTab(ctx.product_data.feedback_url);
-  },
-  
-  createNewSelectedTab: function(url) {
-    chrome.tabs.create({"url": url});
-  }
 };
